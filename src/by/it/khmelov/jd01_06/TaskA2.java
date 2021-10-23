b57faa961ec995461159df5ad7336fbeb06e7586
Alexander Khmelov <a.khmelev@gmail.com>
2021-10-23 21:38:46 +0300

    jd01_06 TaskA test git 2.30 min busybox

--- src/by/it/khmelov/jd01_06/TaskA2.java
+++ src/by/it/khmelov/jd01_06/TaskA2.java
@@ -20,6 +20,12 @@ public class TaskA2 {
         printResult();
     }
 
+    private static void printResult() {
+        for (int i = 0; i < words.length; i++) {
+            System.out.printf("%s=%d\n", words[i], counts[i]);
+        }
+    }
+
     private static void process(String word) {
         for (int i = 0; i < words.length; i++) {
             if (words[i].equals(word)) {
@@ -31,11 +37,5 @@ public class TaskA2 {
         counts = Arrays.copyOf(counts, counts.length + 1);
         words[words.length - 1] = word;
         counts[counts.length - 1] = 1;
-    }
-
-    private static void printResult() {
-        for (int i = 0; i < words.length; i++) {
-            System.out.printf("%s=%d\n", words[i], counts[i]);
-        }
     }
 }
