332f12ad81f86cefbec236e11c5b26f06e8a3c4f
Alexander Khmelov <akhmelev@gmail.com>
2021-10-23 12:24:57 +0300

    Solve jd01_06 Level A

--- src/by/it/khmelov/jd01_05/TaskB.java
+++ src/by/it/khmelov/jd01_05/TaskB.java
@@ -1,4 +1,16 @@
 package by.it.khmelov.jd01_05;
 
 public class TaskB {
+    public static void main(String[] args) {
+        String s = """
+                 fghdffg dfkjghddfjkfh gkdf g g df
+                  gddf dg df g df dfdg dfg dfg dfg ddf
+                  
+                  d fdgdfg df gdfg dfgdfg dffgdffg
+                  akhmelev@mail.ru 
+                  billgates@host.com 
+                                 
+                                
+                """;
+    }
 }
