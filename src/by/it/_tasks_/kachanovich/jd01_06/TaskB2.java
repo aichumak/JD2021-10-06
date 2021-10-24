e1cee386b9ad4ceadda6e4543f4bb7431d0a2d98
MikolaKachanovich <MikolaKachanovich@gmail.com>
2021-10-24 23:15:06 +0300

    jd06_01 B2

--- src/by/it/_tasks_/kachanovich/jd01_06/TaskB2.java
+++ src/by/it/_tasks_/kachanovich/jd01_06/TaskB2.java
@@ -1,25 +1,45 @@
 package by.it._tasks_.kachanovich.jd01_06;
 
-import java.util.Arrays;
+
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TaskB2 {
+
+
+    static void printArray(String[] array) {
+        for (String s : arrayText) {
+            System.out.println(s);
+        }
+    }
+
     static String[] arrayText;
 
-    //    Вывести через \n все предложения текста в порядке возрастания количества символов
-//            (!) в каждом из них. В предложениях нужно сначала заменить все небуквенные символы и их
-//    последовательности на один пробел и выполнить trim() для каждого предложения.
     public static void main(String[] args) {
         String text = Poem.text;
-        arrayText = text.split("\\n|\\.$");
+        arrayText = text.split("\\.\\n");
         Pattern pattern = Pattern.compile("[^а-яА-ЯЁё]{1,4}");
         for (int i = 0; i < arrayText.length; i++) {
             Matcher matcher = pattern.matcher(arrayText[i]);
-            while (matcher.find()) {
-
+            arrayText[i] = matcher.replaceAll(" ");
+        }
+        Pattern pattern1 = Pattern.compile("\\.$");
+        for (int i = 0; i < arrayText.length; i++) {
+            Matcher matcher1 = pattern1.matcher(arrayText[i]);
+            arrayText[i] = matcher1.replaceAll("");
+        }
+        String strMin;
+        for (int i = 0; i < arrayText.length; i++) {
+            arrayText[i].trim();
+            for (int j = 0; j < arrayText.length; j++) {
+                if (arrayText[i].length() < arrayText[j].length()) {
+                    strMin = arrayText[i];
+                    arrayText[i] = arrayText[j];
+                    arrayText[j] = strMin;
+                }
             }
         }
-
+        printArray(arrayText);
     }
 }
