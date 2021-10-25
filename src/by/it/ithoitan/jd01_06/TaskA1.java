5961ab2cdd361591a57c7800fd7935920c45dae5
Pavel Goytan <3160398@gmail.com>
2021-10-26 00:21:03 +0300

    Solve jd2021-10-06 practice 6 lev a1,a2

--- src/by/it/ithoitan/jd01_06/TaskA1.java
+++ src/by/it/ithoitan/jd01_06/TaskA1.java
@@ -6,13 +6,16 @@ import java.util.regex.Pattern;
 public class TaskA1 {
     public static void main(String[] args) {
         StringBuilder modyfyText = new StringBuilder(Poem.text);
-        Pattern pattern=Pattern.compile("[а-яА-ЯёЁ]");
+        Pattern pattern=Pattern.compile("[а-яА-ЯёЁ]{4,}");
         Matcher matcher = pattern.matcher(modyfyText);
         while (matcher.find()){
             int startWordPosition = matcher.start();
             int nextSymbolPosition = matcher.end();
             int length = nextSymbolPosition - startWordPosition;
             modyfyText.setCharAt(startWordPosition+3,'#');
+            if(length>=7){
+                modyfyText.setCharAt(startWordPosition+6, '#');
+            }
 
         }
 
