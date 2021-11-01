0a6e2d6c9e547fa3fd3802c772e24111d29cbf78
Dima Layeuski <dima.layeuski@gmail.com>
2021-11-01 23:30:14 +0300

    completed tasks A for 6th and 7th lesson

--- src/by/it/laevskiy/jd01_07/Vector.java
+++ src/by/it/laevskiy/jd01_07/Vector.java
@@ -1,4 +1,31 @@
 package by.it.laevskiy.jd01_07;
 
-class Vector extends Var{
+import java.util.Arrays;
+
+class Vector extends Var {
+    private double[] values;
+
+    Vector(double[] value) {
+        this.values = Arrays.copyOf(values,values.length);
+    }
+
+    Vector(Vector vector) {
+        this.values = vector.values;
+    }
+
+//    Vector(String strVector) {
+//
+//    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb=new StringBuilder("{");
+        String delimiter="";
+        for (double element:values) {
+            sb.append(delimiter).append(element);
+            delimiter=", ";
+        }
+        sb.append("}");
+        return sb.toString();
+    }
 }
