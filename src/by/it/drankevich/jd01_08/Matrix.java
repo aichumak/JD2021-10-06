a8104f1bd25cceef37fa2046d30b458cf13c2682
Alexandr Drankevich <a.a.drankevich@gmail.com>
2021-11-01 11:38:59 +0300

    solved 8  a,b,c

--- src/by/it/drankevich/jd01_08/Matrix.java
+++ src/by/it/drankevich/jd01_08/Matrix.java
@@ -32,6 +32,150 @@ class Matrix extends Var {
 
     }
 
+    @Override
+    public Var add(Var other) {
+
+        if ((other instanceof Scalar)) {
+            double[][] res = copymatrix(value);
+            for (int i = 0; i < res.length; i++) {
+                for (int j = 0; j < res[0].length; j++) {
+
+                    res[i][j] = res[i][j] + ((Scalar) other).getValue();
+                }
+            }
+            return new Matrix(res);
+        }
+
+        if ((other instanceof Matrix)) {
+            double[][] res = copymatrix(value);
+            if (res[0].length != ((Matrix) other).value[0].length &&
+                    res.length != ((Matrix) other).value.length) {
+                System.out.printf("Incorrect operation %s + %s%n", this, other);
+            } else {
+                for (int i = 0; i < res.length; i++) {
+                    for (int j = 0; j < res[0].length; j++) {
+                        res[i][j] = res[i][j] + ((Matrix) other).value[i][j];
+                    }
+
+                }
+                return new Matrix(res);
+            }
+        }
+
+        return super.add(other);
+
+    }
+
+    @Override
+    public Var sub(Var other) {
+        if ((other instanceof Scalar)) {
+            double[][] res = copymatrix(value);
+            for (int i = 0; i < res.length; i++) {
+                for (int j = 0; j < res[0].length; j++) {
+
+                    res[i][j] = res[i][j] - ((Scalar) other).getValue();
+                }
+            }
+            return new Matrix(res);
+        }
+
+        if ((other instanceof Matrix)) {
+            double[][] res = copymatrix(value);
+            if (res[0].length != ((Matrix) other).value[0].length &&
+                    res.length != ((Matrix) other).value.length) {
+                System.out.printf("Incorrect operation %s - %s%n", this, other);
+            } else {
+                for (int i = 0; i < res.length; i++) {
+                    for (int j = 0; j < res[0].length; j++) {
+                        res[i][j] = res[i][j] - ((Matrix) other).value[i][j];
+                    }
+
+                }
+                return new Matrix(res);
+            }
+        }
+
+        return super.sub(other);
+
+    }
+
+    @Override
+    public Var mul(Var other) {
+        if ((other instanceof Scalar)) {
+            double[][] res = copymatrix(value);
+            for (int i = 0; i < res.length; i++) {
+                for (int j = 0; j < res[0].length; j++) {
+
+                    res[i][j] = res[i][j] * ((Scalar) other).getValue();
+                }
+            }
+            return new Matrix(res);
+        }
+
+        if ((other instanceof Matrix othermatrix)) {
+            double[][] res = copymatrix(value);
+            if (res[0].length != ((Matrix) other).value.length) {
+                System.out.printf("Incorrect operation %s * %s%n", this, other);
+            } else {
+                double[][] newres = new double[res.length][othermatrix.value[0].length];
+
+                for (int i = 0; i < res.length; i++) {
+                    for (int j = 0; j < othermatrix.value[0].length; j++) {
+                        for (int k = 0; k < othermatrix.value.length; k++) {
+                            newres[i][j] = newres[i][j] + res[i][k] * othermatrix.value[k][j];
+
+                        }
+
+                    }
+
+                }
+                return new Matrix(newres);
+            }
+        }
+
+        if ((other instanceof Vector othervector)) {
+            double[][] res = copymatrix(value);
+            if (res[0].length != othervector.getValues().length) {
+                System.out.printf("Incorrect operation %s * %s%n", this, other);
+            } else {
+                double[] newres = new double[res.length];
+
+                for (int i = 0; i < res.length; i++) {
+                    for (int j = 0; j < othervector.getValues().length; j++) {
+
+                        newres[i] = newres[i] + res[i][j] * othervector.getValues()[j];
+
+                    }
+
+                }
+                return new Vector(newres);
+            }
+        }
+
+
+        return super.mul(other);
+    }
+
+    @Override
+    public Var div(Var other) {
+        if ((other instanceof Scalar)) {
+            if (((Scalar) other).getValue() == 0) {
+                System.out.printf("Division by zero %s / %s%n", this, other);
+            } else {
+                double[][] res = copymatrix(value);
+                for (int i = 0; i < res.length; i++) {
+                    for (int j = 0; j < res[0].length; j++) {
+
+                        res[i][j] = res[i][j] / ((Scalar) other).getValue();
+                    }
+                }
+                return new Matrix(res);
+            }
+        }
+
+            return super.div(other);
+        }
+
 
     @Override
     public String toString() {
@@ -55,4 +199,19 @@ class Matrix extends Var {
 
         return sb.toString();
     }
+
+
+    private double[][] copymatrix(double[][] value) {
+        double[][] copymatrix = new double[value.length][value[0].length];
+        for (int i = 0; i < value.length; i++) {
+            for (int j = 0; j < value[0].length; j++) {
+                copymatrix[i][j] = value[i][j];
+
+            }
+
+        }
+        return copymatrix;
+    }
 }
+
+
