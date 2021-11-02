ccbaa27a4a4c7a9df237b33cc361e4150c003c07
VladimirSerkutyev <serkutyevv@gmail.com>
2021-11-02 22:36:40 +0300

    Homework lesson 6: Completed C1 task          lesson 7: Completed A, B and C tasks

---
+++ src/by/it/serkutsyev/jd01_07/Scalar.java
@@ -0,0 +1,27 @@
+package by.it.serkutsyev.jd01_07;
+
+public class Scalar extends Var {
+
+    private double value;
+
+    public Scalar(double value) {
+        this.value = value;
+    }
+
+    public Scalar(Scalar scalar) {
+        this.value = scalar.value;
+    }
+
+    public Scalar(String strScalar) {
+        this.value = Double.parseDouble(strScalar);
+    }
+
+    @Override
+    public String toString() {
+        return Double.toString(value);
+    }
+
+    {
+
+    }
+}
