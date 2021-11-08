c516217a3252fad4a1115cadea16b24099798c8a
Alexandr Drankevich <a.a.drankevich@gmail.com>
2021-11-08 16:35:38 +0300

    10 a,b,c

--- src/by/it/drankevich/jd01_10/BeanTester.java
+++ src/by/it/drankevich/jd01_10/BeanTester.java
@@ -1,24 +1,21 @@
 package by.it.drankevich.jd01_10;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 
 public class BeanTester {
     public static void main(String[] args) throws Exception {
         Class<Bean> beanClass = Bean.class;
         Bean bean1 = beanClass.getDeclaredConstructor().newInstance();
-        Class<Param> paramClass = Param.class;
-        Method a = paramClass.getMethod("a");
-        Method b = paramClass.getMethod("b");
+
 
 
         Method[] methods = beanClass.getDeclaredMethods();
         for (Method method : methods) {
 
-            if (method.isAnnotationPresent(paramClass)) {
-                Annotation annotation = method.getAnnotation(paramClass);
-                int avalue = (int) a.invoke(annotation);
-                int bvalue = (int) b.invoke(annotation);
+            if (method.isAnnotationPresent(Param.class)) {
+                Param annotation = method.getAnnotation(Param.class);
+                int avalue = annotation.a();
+                int bvalue = annotation.b();
 
                 System.out.println(method.getName() + " " + method.invoke(bean1, avalue, bvalue));
 
