cc56641a34bdbc59b9062c3d4736bc415ef36e0b
EvgenyKonon <konan.jack@gmail.com>
2021-11-11 12:46:14 +0300

    fix _classwork_

---
+++ src/by/it/_classwork_/jd01_08/Vector.java
@@ -0,0 +1,52 @@
+package by.it._classwork_.jd01_08;
+
+import java.util.Arrays;
+
+class Vector extends Var {
+
+    private final double[] values;
+
+    public double[] getValues() {
+        return Arrays.copyOf(values, values.length);
+    }
+
+    public Vector(double[] values) {
+        this.values = Arrays.copyOf(values, values.length);
+    }
+
+    @Override
+    public Var add(Var other) {
+        if (other instanceof Scalar otherScalar) {
+            double[] result = Arrays.copyOf(values, values.length);
+            for (int i = 0; i < result.length; i++) {
+                result[i] = result[i] + otherScalar.getValue();
+            }
+            return new Vector(result);
+        }
+        if (other instanceof Vector otherVector) {
+            double[] result = Arrays.copyOf(values, values.length);
+            //TODO throw Exception Incorrect operation
+            if (values.length!=otherVector.values.length) {
+                System.out.printf("Incorrect operation %s / %s%n", this, other);
+            }
+            for (int i = 0; i < result.length; i++) {
+                result[i] = result[i] + otherVector.values[i];
+            }
+            return new Vector(result);
+        }
+        return super.add(other);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder out = new StringBuilder();
+        out.append("{");
+        String delimiter = "";
+        for (double value : values) {
+            out.append(delimiter).append(value);
+            delimiter = ", ";
+        }
+        out.append("}");
+        return out.toString();
+    }
+}
