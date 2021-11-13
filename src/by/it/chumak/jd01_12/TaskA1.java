00123684489df9dd9410393a780f4d5aa500f4b1
aichumak.dev@gmail.com <aichumak.dev@gmail.com>
2021-11-13 17:29:28 +0300

    Solve jd01_12 Level A

--- src/by/it/chumak/jd01_12/TaskA1.java
+++ src/by/it/chumak/jd01_12/TaskA1.java
@@ -7,13 +7,17 @@ import java.util.Random;
 public class TaskA1 {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
+        TaskA1 taskA1 = new TaskA1();
         Random random = new Random();
-
-
-
+        for (int i = 0; i < 20; i++) {
+            list.add(1 + random.nextInt(10));
+        }
+        System.out.println(list);
+        taskA1.clearBad(list);
+        System.out.println(list);
     }
 
     void clearBad(List<Integer> grades) {
-
+        grades.removeIf(grade -> grade < 4);
     }
 }
--- src/by/it/chumak/jd01_12/TaskA2.java
+++ src/by/it/chumak/jd01_12/TaskA2.java
@@ -1,8 +1,31 @@
 package by.it.chumak.jd01_12;
 
+import java.util.*;
+
 public class TaskA2 {
     public static void main(String[] args) {
+        List<Integer> list1 = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> list2 = Arrays.asList(4, 5, 6, 7, 8, 9);
 
+        Set<Integer> hashSet = new HashSet<>(list1);
+        Set<Integer> treeSet = new TreeSet<>(list2);
+
+        Set<Integer> union = getUnion(hashSet, treeSet);
+        System.out.println(union);
+
+        Set<Integer> cross = getCross(hashSet, treeSet);
+        System.out.println(cross);
     }
 
+    private static Set<Integer> getUnion(Set<Integer> a, Set<Integer> b) {
+        HashSet<Integer> result = new HashSet<>(a);
+        result.addAll(b);
+        return result;
+    }
+
+    private static Set<Integer> getCross(Set<Integer> a, Set<Integer> b) {
+        HashSet<Integer> result = new HashSet<>(a);
+        result.retainAll(b);
+        return result;
+    }
 }
