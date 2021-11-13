19da2baa4f29575966c39e6346fcbcea2c3234d3
Alexander Khmelov <a.khmelev@gmail.com>
2021-11-13 12:46:52 +0300

    jd01_12 classwork

---
+++ src/by/it/_classwork_/calc/Matrix.java
@@ -0,0 +1,13 @@
+package by.it._classwork_.calc;
+
+class Matrix extends Var {
+
+    public Matrix(String stringValues) {
+        super();
+    }
+
+    @Override
+    public String toString() {
+        return "Matrix{}";
+    }
+}
