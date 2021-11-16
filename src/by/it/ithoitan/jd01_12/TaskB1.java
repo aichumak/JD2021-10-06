48ba873106b164819a99fbd6512aae977905b5a9
Pavel Goytan <3160398@gmail.com>
2021-11-16 03:10:11 +0300

    01_12 a,b1

--- src/by/it/ithoitan/jd01_12/TaskB1.java
+++ src/by/it/ithoitan/jd01_12/TaskB1.java
@@ -1,15 +1,53 @@
 package by.it.ithoitan.jd01_12;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
+
+
 public class TaskB1 {
     public static void main(String[] args) {
-        Map<String, String> hashmap = new HashMap<>();
+        Map<String, Integer> hashmap = new HashMap<>();
         Scanner scanner = new Scanner(System.in);
+        StringBuilder builder = new StringBuilder();
         for (; ; ) {
             String wordEnglish = scanner.nextLine();
+            String trim = wordEnglish.trim();
+            String s2 = trim.replaceAll("[\\n]","");
+            builder.append(s2).append(" ");
+            // System.out.println(builder);
+            String[] s = builder.toString().split("[().,!?:;\\s]");
+            //System.out.println(Arrays.toString(s));
+            // String all = wordEnglish.trim().replaceAll("\\W", " ");
+            // String[] s = all.split(" ");
+            //  System.out.println(Arrays.toString(s));
+            if (wordEnglish.equals("end")) {
+                for (String s1 : s) {
+                    Integer count = hashmap.get(s1);
+                    if (count == null) {
+                        count = 0;
+                    }
+                    hashmap.put(s1, count + 1);
+                }
+                System.out.println(hashmap);
+                break;
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+/**
+        for (; ; ) {
             hashmap.put(wordEnglish,wordEnglish);
             if (wordEnglish.equals("end")) {
                 break;
@@ -22,6 +60,6 @@ public class TaskB1 {
                 }
             }
 
-        }
-    }
-}
+        } */
+
+
