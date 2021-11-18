4447e86f313d39999eb9ffa4944424b709c6cb70
aichumak.dev@gmail.com <aichumak.dev@gmail.com>
2021-11-18 22:14:15 +0300

    Solve jd01_13 Level C

--- src/by/it/chumak/jd01_13/TaskC.java
+++ src/by/it/chumak/jd01_13/TaskC.java
@@ -1,7 +1,55 @@
 package by.it.chumak.jd01_13;
 
+import java.util.*;
+
 public class TaskC {
+    static Stack<Double> doubleStack = new Stack<>();
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
 
+        int countErrors = 0;
+        readData();
+
+
+    }
+
+    static void readData() {
+        int countErrors = 0;
+
+        while (countErrors < 6) {
+            try {
+                String text = sc.nextLine();
+                List<String> arrayList = List.of(text.split(" "));
+                for (String element : arrayList) {
+                    try {
+                        doubleStack.add(Double.valueOf(element));
+                    } catch (NumberFormatException e) {
+                        throw e;
+                    }
+                }
+                //readData();
+            } catch (InputMismatchException | NullPointerException | NumberFormatException e) {
+
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+                countErrors++;
+                StringJoiner stringJoiner = new StringJoiner(" ");
+                Stack<Double> bufferStack = (Stack<Double>) doubleStack.clone();
+                bufferStack.sort(Collections.reverseOrder());
+                Iterator<Double> iterator = bufferStack.iterator();
+                while (iterator.hasNext()) {
+                    stringJoiner.add(iterator.next().toString());
+                }
+                System.out.println(stringJoiner);
+
+                if (countErrors == 5) {
+                    throw new Exception;
+                }
+            }
+        }
     }
 }
