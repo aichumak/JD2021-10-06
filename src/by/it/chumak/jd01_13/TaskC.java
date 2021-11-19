85ca5c7ceb9bb5942fdd870e333be645eb7814c3
aichumak.dev@gmail.com <aichumak.dev@gmail.com>
2021-11-19 13:12:30 +0300

    Solve jd01_13 Level C

--- src/by/it/chumak/jd01_13/TaskC.java
+++ src/by/it/chumak/jd01_13/TaskC.java
@@ -6,46 +6,46 @@ public class TaskC {
     static Stack<Double> doubleStack = new Stack<>();
     static Scanner sc;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         sc = new Scanner(System.in);
         readData();
     }
 
-    static void readData() {
+    static void readData() throws InterruptedException {
         int countErrors = 0;
-        String text = "";
+        String inputText;
 
         while (countErrors < 6) {
-
-            try {
-                text = sc.next();
-            } catch (NoSuchElementException | IllegalStateException ex) {
-                countErrors++;
-            }
-
-            List<String> arrayList = List.of(text.split(" "));
+            inputText = sc.next();
+            List<String> arrayList = List.of(inputText.split(" "));
             for (String element : arrayList) {
                 if (countErrors == 5) {
-                    throw new RuntimeException("123");
+                    throw new RuntimeException("incorrect input");
                 }
                 try {
                     doubleStack.add(Double.valueOf(element));
-                } catch (NumberFormatException e) {
-                    try {
-                        Thread.sleep(100);
-                    } catch (InterruptedException ex) {
-                        //ex.printStackTrace();
-                    }
+                } catch (NumberFormatException | NoSuchElementException | IllegalStateException e) {
+                    sleep();
                     countErrors++;
-                    StringJoiner stringJoiner = new StringJoiner(" ");
-                    Stack<Double> bufferStack = (Stack<Double>) doubleStack.clone();
-                    bufferStack.sort(Collections.reverseOrder());
-                    for (Double aDouble : bufferStack) {
-                        stringJoiner.add(aDouble.toString());
-                    }
-                    System.out.println(stringJoiner);
+                    printException();
                 }
             }
         }
+    }
+
+    @SuppressWarnings("unchecked")
+    private static void printException() {
+        StringJoiner stringJoiner = new StringJoiner(" ");
+        Stack<Double> bufferStack = (Stack<Double>) doubleStack.clone();
+        bufferStack.sort(Collections.reverseOrder());
+
+        for (Double aDouble : bufferStack) {
+            stringJoiner.add(aDouble.toString());
+        }
+        System.out.println(stringJoiner);
+    }
+
+    private static void sleep() throws InterruptedException {
+        Thread.sleep(100);
     }
 }
