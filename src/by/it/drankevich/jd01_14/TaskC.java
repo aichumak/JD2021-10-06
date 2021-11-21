7cf34a26080d8a33ad5e2313e9344fc8baf32123
AD <a.a.drankevich@gmail.com>
2021-11-21 19:28:44 +0300

    14 a,b,c

--- src/by/it/drankevich/jd01_14/TaskC.java
+++ src/by/it/drankevich/jd01_14/TaskC.java
@@ -1,33 +1,58 @@
 package by.it.drankevich.jd01_14;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 public class TaskC {
     public static void main(String[] args) {
-        String path = System.getProperty("user.dir")+ File.separator+"src"+File.separator+"by"+File.separator+"it" ;
+        String path = System.getProperty("user.dir") + File.separator + "src" + File.separator + "by" + File.separator + "it"
+                + File.separator + "drankevich" + File.separator;
 
-        File a = new File("drankevich");
-        String b=a.getName();
-        String s= a.getAbsolutePath();
+        File dir = new File(path);
 
-        System.out.println(s);
-        System.out.println(b);
-        System.out.println( a.isDirectory());
+        recurs(dir);
 
-
-
-
-
-        String name= TaskC.class.getName().replace(TaskC.class.getSimpleName(),"")
-                .replace(".", File.separator);
-
-
-
-        String path2 = System.getProperty("user.dir")+ File.separator+"src"+File.separator+TaskC.class.getSimpleName() ;
-
-
-        String path1 = System.getProperty("user.dir")+ File.separator+"src"+File.separator+name;
-
-
+        printToFile(dir);
     }
+
+
+    private static void recurs(File dir) {
+
+
+        for (File file : dir.listFiles()) {
+            if (file.isDirectory()) {
+                System.out.printf("dir:%s%n", file.getName());
+                recurs(file);
+            } else {
+                System.out.printf("file:%s%n", file.getName());
+            }
+
+
+        }
+    }
+
+    private static void printToFile(File dir) {
+        String filename = Filenames.getFullPath(TaskC.class, "resultTaskC.txt");
+        try (PrintWriter printWriter = new PrintWriter(filename)) {
+
+            for (File file : dir.listFiles()) {
+                if (file.isDirectory()) {
+                    printWriter.printf("dir:%s%n", file.getName());
+                    printToFile(file);
+                } else {
+                    printWriter.printf("file:%s%n", file.getName());
+
+                }
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
+
+
+
