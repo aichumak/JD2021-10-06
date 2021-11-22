40f5a1322dacad60d1239f18d66475975f14cf27
Pavel Goytan <3160398@gmail.com>
2021-11-23 02:22:41 +0300

    01_14 a

--- src/by/it/ithoitan/jd01_14/TaskA.java
+++ src/by/it/ithoitan/jd01_14/TaskA.java
@@ -1,20 +1,78 @@
 package by.it.ithoitan.jd01_14;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class TaskA {
-    public static void main(String[] args) {
-        String filename = "dataTaskA.bin";
-        String srcProperty = System.getProperty("user.dir")
+    public static String dir (Class<?> cl){
+        String path = System.getProperty("user.dir")
                 + File.separator+"src"+File.separator;
-        Class<?> structureClass = TaskA.class;
+        String clDir = cl.getName().replace(cl.getSimpleName(),"").replace(".",File.separator);
+        return path+clDir;
+    }
+    public static void main(String[] args) {
+        DataOutputStream dos = null;
 
-        String name = structureClass.getName();
-        String simpleName = structureClass.getSimpleName();
-        String relationPath = name
-                .replace(simpleName,"")
-                .replace(".",File.separator);
-        String path = srcProperty+relationPath+filename;
-        System.out.println(path);
+        try {
+            dos = new DataOutputStream(
+                    new BufferedOutputStream(
+                            new FileOutputStream(dir(TaskA.class)+"dataTaskA.bin")));
+            Random random = new Random();
+            for (int i = 0; i < 20; i++) {
+                dos.writeInt(random.nextInt(1000));
+            }
+        } catch (IOException e) {
+            throw  new RuntimeException(e);
+        }
+        finally {
+            if(dos!=null){
+                try {
+                    dos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        ArrayList<Integer> integers = new ArrayList<>();
+        try {
+            DataInputStream input = new DataInputStream(
+                    new BufferedInputStream(
+                        new FileInputStream(dir(TaskA.class)+"dataTaskA.bin")));
+
+
+            while (input.available()>0){
+                integers.add(input.readInt());
+
+            }
+
+
+            double sum = 0;
+            for (Integer integer : integers) {
+                System.out.printf("%s ", integer);
+
+                sum += integer;
+            }
+
+            System.out.println("\navg="+sum/integers.size());
+
+        } catch (IOException e) {
+            throw  new RuntimeException(e);
+        }
+        try {
+            PrintWriter out2 = new PrintWriter(
+                    new FileWriter(dir(TaskA.class)+"resultTaskA.txt"));
+            double sum2 = 0;
+            for (Integer integer : integers) {
+                out2.printf("%s", integer);
+                sum2 += integer;
+            }
+
+            out2.printf("\navg=%f%n",sum2/integers.size());
+        } catch (IOException e) {
+            throw  new RuntimeException(e);
+        }
+
+
     }
 }
