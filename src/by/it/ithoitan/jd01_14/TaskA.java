f7325708e5e5619f85e4f69f82760a7bac56e4ac
Pavel Goytan <3160398@gmail.com>
2021-11-24 01:41:07 +0300

    01_14 a,b

--- src/by/it/ithoitan/jd01_14/TaskA.java
+++ src/by/it/ithoitan/jd01_14/TaskA.java
@@ -5,28 +5,28 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class TaskA {
-    public static String dir (Class<?> cl){
+    public static String dir(Class<?> cl) {
         String path = System.getProperty("user.dir")
-                + File.separator+"src"+File.separator;
-        String clDir = cl.getName().replace(cl.getSimpleName(),"").replace(".",File.separator);
-        return path+clDir;
+                + File.separator + "src" + File.separator;
+        String clDir = cl.getName().replace(cl.getSimpleName(), "").replace(".", File.separator);
+        return path + clDir;
     }
+
     public static void main(String[] args) {
         DataOutputStream dos = null;
 
         try {
             dos = new DataOutputStream(
                     new BufferedOutputStream(
-                            new FileOutputStream(dir(TaskA.class)+"dataTaskA.bin")));
+                            new FileOutputStream(dir(TaskA.class) + "dataTaskA.bin")));
             Random random = new Random();
             for (int i = 0; i < 20; i++) {
                 dos.writeInt(random.nextInt(1000));
             }
         } catch (IOException e) {
-            throw  new RuntimeException(e);
-        }
-        finally {
-            if(dos!=null){
+            throw new RuntimeException(e);
+        } finally {
+            if (dos != null) {
                 try {
                     dos.close();
                 } catch (IOException e) {
@@ -38,10 +38,10 @@ public class TaskA {
         try {
             DataInputStream input = new DataInputStream(
                     new BufferedInputStream(
-                        new FileInputStream(dir(TaskA.class)+"dataTaskA.bin")));
+                            new FileInputStream(dir(TaskA.class) + "dataTaskA.bin")));
 
 
-            while (input.available()>0){
+            while (input.available() > 0) {
                 integers.add(input.readInt());
 
             }
@@ -54,23 +54,22 @@ public class TaskA {
                 sum += integer;
             }
 
-            System.out.println("\navg="+sum/integers.size());
+            System.out.println("\navg=" + sum / integers.size());
 
         } catch (IOException e) {
-            throw  new RuntimeException(e);
+            throw new RuntimeException(e);
         }
         try {
-            PrintWriter out2 = new PrintWriter(
-                    new FileWriter(dir(TaskA.class)+"resultTaskA.txt"));
+            PrintWriter out2 = new PrintWriter(dir(TaskA.class) + "resultTaskA.txt");
             double sum2 = 0;
             for (Integer integer : integers) {
                 out2.printf("%s", integer);
                 sum2 += integer;
             }
 
-            out2.printf("\navg=%f%n",sum2/integers.size());
+            out2.println("\navg=" + sum2 / integers.size());
         } catch (IOException e) {
-            throw  new RuntimeException(e);
+            throw new RuntimeException(e);
         }
 
 
