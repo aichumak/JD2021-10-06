df589cc300d4f1a031e52749683ded7321b31d8c
Dima Layeuski <dima.layeuski@gmail.com>
2021-11-24 17:59:01 +0300

    tasks for 12th and 13th lesson

---
+++ src/by/it/laevskiy/calc/Matrix.java
@@ -0,0 +1,13 @@
+package by.it.laevskiy.calc;
+
+class Matrix extends Var {
+
+    public Matrix(String stringValues) {
+        super();
+    }
+
+    @Override
+    public String toString() {
+        return "Matrix{}";
+    }
+}
