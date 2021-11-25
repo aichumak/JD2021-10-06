1ac8b59ffac3c92c39ee9af19c3ef42ddaf3226e
EvgenyKonon <konan.jack@gmail.com>
2021-11-25 11:11:42 +0300

    _homework_

---
+++ src/by/it/konon/jd14_01/FileNames.java
@@ -0,0 +1,21 @@
+package by.it.konon.jd14_01;
+
+import java.io.File;
+
+public class FileNames {
+    private FileNames(){
+
+    }
+     static String getFullPath(String fileName, Class<?> currentClass) {
+        String srcProject = System.getProperty("user.dir")
+                + File.separator + "src" + File.separator;
+        String name = currentClass.getName();
+        String currentClassSimpleName = currentClass.getSimpleName();
+        String relationPath = name
+                .replace(currentClassSimpleName, "")
+                .replace(".", File.separator);
+
+        return srcProject + relationPath + fileName;
+    }
+}
+
---
+++ src/by/it/konon/jd14_01/TaskB.java
@@ -0,0 +1,7 @@
+package by.it.konon.jd14_01;
+
+public class TaskB {
+    public static void main(String[] args) {
+
+    }
+}
