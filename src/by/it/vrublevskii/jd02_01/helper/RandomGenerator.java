ecdfcd8b0e9fa6a420594e11471090ac24cb6ba1
Viktor <vrubleuski.viktar@gmail.com>
2021-12-01 16:16:56 +0300

    Solved 15

---
+++ src/by/it/vrublevskii/jd02_01/helper/RandomGenerator.java
@@ -0,0 +1,17 @@
+package by.it.vrublevskii.jd02_01.helper;
+
+import java.util.Random;
+
+public class RandomGenerator {
+
+    private static final Random random = new Random();
+
+
+    public static int get(int min, int max) {
+        return min + random.nextInt(max - min + 1);
+    }
+
+    public static int get(int max) {
+        return get(0, max);
+    }
+}
