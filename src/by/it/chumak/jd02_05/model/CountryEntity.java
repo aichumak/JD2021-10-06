9a93ce94240331ba764ca182ef15871dfc0c1d07
aichumak <aichumak.dev@gmail.com>
2021-12-11 19:24:57 +0300

    Solve jd02_05 Task B

--- src/by/it/chumak/jd02_05/model/CountryEntity.java
+++ src/by/it/chumak/jd02_05/model/CountryEntity.java
@@ -3,13 +3,11 @@ package by.it.chumak.jd02_05.model;
 public class CountryEntity {
 
     public String getCountryCodeAlfa2(String languageCode) {
-        if (languageCode.equals("en")) {
-            return "EN";
-        } else if (languageCode.equals("ru")) {
-            return "RU";
-        } else if (languageCode.equals("be")) {
-            return "BY";
-        }
-        return "";
+        return switch (languageCode) {
+            case "en" -> "EN";
+            case "ru" -> "RU";
+            case "be" -> "BY";
+            default -> "";
+        };
     }
 }
