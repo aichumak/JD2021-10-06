b034c9c15ba2f2eda41be35fdae4f479c35591e5
aichumak <aichumak.dev@gmail.com>
2021-12-11 17:47:25 +0300

    Solve jd02_05 Task A

---
+++ src/by/it/chumak/jd02_05/model/ResourceManager.java
@@ -0,0 +1,26 @@
+package by.it.chumak.jd02_05.model;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public enum ResourceManager {
+    INSTANCE;
+
+    public static final String basename = "by.it.chumak.jd02_05.language.strings";
+
+    private ResourceBundle resourceBundle;
+
+    ResourceManager() {
+        setLocale(new Locale("en"));
+    }
+
+    public void setLocale(Locale locale) {
+        resourceBundle = ResourceBundle.getBundle(basename, locale);
+    }
+
+    public String get(String key) {
+        return resourceBundle.getString(key);
+    }
+
+    ;
+}
