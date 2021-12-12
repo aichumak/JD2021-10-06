b9e5cd603db91d62b9d5f6ce6208b60f5f00f707
Dzianis <d.v.brutski@gmail.com>
2021-12-12 23:34:50 +0300

    exercise jd01_12(level B1, B2); jd02_03(level A).

---
+++ src/by/it/brutski/jd02_03/entity/Cashier.java
@@ -0,0 +1,27 @@
+package by.it.brutski.jd02_03.entity;
+
+public class Cashier {
+    private final String name;
+    private double total;
+
+    public Cashier(int number) {
+        this.name = "Cashier # " + number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    @Override
+    public String toString() {
+        return "\t" + name + " total = " + total;
+    }
+}
