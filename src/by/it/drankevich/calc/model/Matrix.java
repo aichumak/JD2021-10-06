ec6859f5e8a02cdbf81d7a6e674080031f60efc9
AD <a.a.drankevich@gmail.com>
2021-12-13 18:41:29 +0300

    jd02_05 a,b,c

--- src/by/it/drankevich/calc/model/Matrix.java
+++ src/by/it/drankevich/calc/model/Matrix.java
@@ -18,34 +18,38 @@ public class Matrix extends Var {
         this.value = matrix.value;
     }
 
-    public Matrix(String strMatrix) {
-        String[]str=strMatrix.split("\\}\\,[ ]?\\{");
-        int arrayx=str.length;
+    public Matrix(String strMatrix) throws CalcExeption {
+        try {
+            String[] str = strMatrix.split("\\}\\,[ ]?\\{");
+            int arrayx = str.length;
 
-        String str0 = str[0].replace('{', ' ');
-        String str1 = str0.replace('}', ' ');
-        String str2 = str1.trim();
-        String[] arraystr = str2.split("[,\\s]+");
-        int arrayy=arraystr.length;
+            String str0 = str[0].replace('{', ' ');
+            String str1 = str0.replace('}', ' ');
+            String str2 = str1.trim();
+            String[] arraystr = str2.split("[,\\s]+");
+            int arrayy = arraystr.length;
 
-        String strMatrix1 = strMatrix.replace('{', ' ');
-        String strMatrix2 = strMatrix1.replace('}', ' ');
-        String strMatrix3 = strMatrix2.trim();
-        String[] strMatrixarr = strMatrix3.split("[,\\s]+");
+            String strMatrix1 = strMatrix.replace('{', ' ');
+            String strMatrix2 = strMatrix1.replace('}', ' ');
+            String strMatrix3 = strMatrix2.trim();
+            String[] strMatrixarr = strMatrix3.split("[,\\s]+");
 
-        double[][] array1 = new double[arrayx][ arrayy];
-        int k = 0;
+            double[][] array1 = new double[arrayx][arrayy];
+            int k = 0;
 
-        for (int i = 0; i < arrayx; i++) {
+            for (int i = 0; i < arrayx; i++) {
 
-            for (int j = 0; j < arrayy; j++) {
-                array1[i][j] = Double.parseDouble(strMatrixarr[k]);
-                k++;
+                for (int j = 0; j < arrayy; j++) {
+                    array1[i][j] = Double.parseDouble(strMatrixarr[k]);
+                    k++;
+                }
+
+
             }
-
-
+            this.value = array1;
+        } catch (ArrayIndexOutOfBoundsException e){
+            throw new CalcExeption(ResourceManager.get("massage.Stupid"));
         }
-        this.value = array1;
 
 
     }
