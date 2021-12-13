669e3d88c00b9b85b0d98625c390d14de1f5901f
Анастасия <!kemer2020>
2021-12-13 19:23:15 +0300

    A homework 02_05

---
+++ src/by/it/kanaplianik/jd02_05/MessageText.java
@@ -0,0 +1,8 @@
+package by.it.kanaplianik.jd02_05;
+
+public interface MessageText {
+
+    String WELCOME = "message.welcome";
+    String QUESTION = "message.question";
+    String ANSWER = "message.answer";
+}
---
+++ src/by/it/kanaplianik/jd02_05/User.java
@@ -0,0 +1,9 @@
+package by.it.kanaplianik.jd02_05;
+
+public interface User {
+
+        String I_WORD="message.iword";
+        String FIRST_NAME="message.firstname";
+        String LAST_NAME="message.lastname";
+
+}
