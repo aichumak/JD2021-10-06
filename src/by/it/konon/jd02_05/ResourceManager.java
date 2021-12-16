5206b7551d8197a000d1ec33c9da646af41d0873
EvgenyKonon <konan.jack@gmail.com>
2021-12-16 21:48:50 +0300

    _homework_

--- src/by/it/konon/jd02_05/ResourceManager.java
+++ src/by/it/konon/jd02_05/ResourceManager.java
@@ -1,26 +1,32 @@
-package by.it._classwork_.jd02_05;
+package by.it.konon.jd02_05;
 
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 public enum ResourceManager {
-    INSTANCE;
 
-    public static final String basename = "by.it._classwork_.jd02_05.language.strings";
+    INSTANCE;
+    private Locale locale;
+
+    public static final String basename = "by.it.konon.jd02_05.language.strings";
 
     private ResourceBundle resourceBundle;
 
     ResourceManager() {
         setLocale(new Locale("en"));
+
     }
 
     public void setLocale(Locale locale) {
         resourceBundle = ResourceBundle.getBundle(basename, locale);
+        this.locale = locale;
     }
 
     public String get(String key) {
         return resourceBundle.getString(key);
     }
 
-    ;
+    public Locale getLocale(){
+        return locale;
+    }
 }
