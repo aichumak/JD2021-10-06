b3c38af6231d11ac82f37ddd6d4e8adf21c4acc0
Alexey <astapchiktranslate@gmail.com>
2021-12-17 01:57:54 +0200

    Homework: 02_01 - 02_05

--- src/by/it/astapchik/jd02_05/ResourceManager.java
+++ src/by/it/astapchik/jd02_05/ResourceManager.java
@@ -6,20 +6,22 @@ import java.util.ResourceBundle;
 
 public enum ResourceManager {
     INSTANCE;
-    public static final String basename = "by.it.astapchik.jd02_05.stringsResource.strings";
-
+    public static final String BASENAME = "by.it.astapchik.jd02_05.stringsResource.strings";
+    private Locale locale;
     private ResourceBundle resourceBundle;
 
     ResourceManager() {
-        setLocale(new Locale("de", "DE"));
+        setLocale(new Locale("ru", "RU"));
     }
 
     public void setLocale(Locale locale) {
-        resourceBundle = ResourceBundle.getBundle(basename, locale);
+        this.locale = locale;
+        this.resourceBundle = ResourceBundle.getBundle(BASENAME, this.locale);
     }
 
     public String get(String key){
         return resourceBundle.getString(key);
     }
+
 }
 
