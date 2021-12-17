0c0bc5ede9ce2c5f4f9cb36cdaf53cc600880cf1
Dzianis <d.v.brutski@gmail.com>
2021-12-17 21:11:08 +0300

    exercise jd02_05(level A); jd02_06(level A).

---
+++ src/by/it/brutski/jd02_05/Messages.java
@@ -0,0 +1,7 @@
+package by.it.brutski.jd02_05;
+
+public interface Messages {
+    String WELCOME = "messages.welcome";
+    String QUESTION = "messages.question";
+    String SENTENCE = "messages.sentence";
+}
